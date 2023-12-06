9d93d02c74eddc7e7f2b985a9ef472440a896e15
Gawei <okm2996088@gmail.com>
2023-12-06 09:43:58 +0800

    訂單系統登入判斷OK

--- js/list.js
+++ js/list.js
@@ -1,22 +1,45 @@
-$(function(){
-    $("#check").change(function(){
+var user = { "listname": "20231204", "tel": "098888111" };
+var isAgree;
+$(function () {
+
+    $("#sw1").change(function () {
         console.log($(this).val());
-        if($(this).is(':checked')){
+        if ($(this).is(':checked')) {
             console.log("同意");
-        }else{
+            $(this).next().text("已確認資料");
+            isAgree = true;
+        }
+        else {
             console.log("不同意");
+            $(this).next().text("未確認資料");
+            isAgree = false;
         }
     });
 
+    $("#OK_btn").bind("click", function () {
+        if (isAgree == true) {
+            var listnames = Number($("#listname").val()); //輸入的
+            var tel = $("#tel").val();
+            // console.log(typeof Number(user["listname"]));
+            if (listnames == Number(user["listname"])) {
+                if (tel == user.tel) {
+                    alert("成功");
+                } else {
+                    alert("電話號碼錯誤");
+                }
+            } else {
+                alert("訂單編號錯誤");
+            }
+        }else{
+            alert("檢查訂單資料");
+        }
 
-    $("#OK_btn").bind("click",function(){
-        console.log($("#listname").val());
-        console.log($("#tel").val());
-        console.log($("#buyday").val());
-        
+
     });
 
 
 
 
 });
+
+
